18caff28a77d7873db2da5a9c2d470de874a70ba
Gaetan Perrot <gaetan.perrot@spacecubics.com>
2024-04-05 18:43:52 +0900

    Add tests for buffer_clone
    This commit adds test for csp_buffer_clone .
    
    Signed-off-by: Gaetan Perrot <gaetan.perrot@spacecubics.com>

--- src/buffer.c
+++ src/buffer.c
@@ -58,4 +58,54 @@ ZTEST(buffer, test_buffer_over_allocate)
 	}
 }
 
+ZTEST(buffer, test_buffer_clone_whithout_header)
+{
+	csp_packet_t *packet, *clone = NULL;
+	int ret;
+
+	packet = csp_buffer_get(0);
+	zassert_true(packet != NULL, NULL);
+
+	memcpy(packet->data, "Hello", 5);
+	packet->length = 5;
+
+	/* First try clone without header */
+	clone = csp_buffer_clone(packet);
+	zassert_not_null(clone, "Failed to clone CSP buffer");
+	zassert_equal(packet->length, clone->length, "Packet and clone have different length");
+	ret = strcmp(packet->data, clone->data);
+	zassert_equal(ret, 0, "Packet and clone have different data");
+
+	csp_buffer_free(packet);
+	csp_buffer_free(clone);
+}
+
+ZTEST(buffer, test_buffer_clone_with_header)
+{
+	csp_packet_t *packet, *clone = NULL;
+	int ret;
+
+	packet = csp_buffer_get(0);
+	zassert_true(packet != NULL, NULL);
+
+	memcpy(packet->data, "Hello", 5);
+	packet->length = 5;
+
+	/* Try clone with header */
+	csp_id_prepend(packet);
+	clone = csp_buffer_clone(packet);
+	zassert_not_null(clone, "Failed to clone CSP buffer");
+	zassert_equal(packet->length, clone->length, "Packet and clone have different length");
+	/* Should also test that packet->frame_begin are different pointer */
+	/* zassert_not_equal(packet->frame_begin,clone->frame_begin, "Packet and clone have the same
+	 * frame_begin"); */
+	zassert_equal(packet->frame_length, clone->frame_length,
+		      "Packet and clone have different frame_length");
+	ret = strcmp(packet->data, clone->data);
+	zassert_equal(ret, 0, "Packet and clone have different data");
+
+	csp_buffer_free(packet);
+	csp_buffer_free(clone);
+}
+
 ZTEST_SUITE(buffer, NULL, setup, NULL, NULL, NULL);
