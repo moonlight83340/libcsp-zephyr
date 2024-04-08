be3452554371a7e86d93c113679e872268ff8cec
Gaetan Perrot <gaetan.perrot@spacecubics.com>
2024-04-08 16:41:11 +0900

    Add test for csp_buffer_get_isr
    This commit adds test for csp_buffer_get_isr .
    
    Signed-off-by: Gaetan Perrot <gaetan.perrot@spacecubics.com>

--- src/buffer.c
+++ src/buffer.c
@@ -58,4 +58,49 @@ ZTEST(buffer, test_buffer_over_allocate)
 	}
 }
 
+ZTEST(buffer, test_buffer_isr_count)
+{
+	csp_packet_t *packets[CSP_BUFFER_COUNT];
+	int i;
+
+	memset(packets, 0, sizeof(packets));
+
+	zassert_true(csp_buffer_remaining() == CSP_BUFFER_COUNT);
+
+	for (i = 0; i < CSP_BUFFER_COUNT; i++) {
+		packets[i] = csp_buffer_get_isr(0);
+		zassert_true(packets[i] != NULL, NULL);
+	}
+
+	zassert_true(csp_buffer_remaining() == 0);
+
+	for (i = 0; i < CSP_BUFFER_COUNT; i++) {
+		csp_buffer_free_isr(packets[i]);
+	}
+
+	zassert_true(csp_buffer_remaining() == CSP_BUFFER_COUNT);
+}
+
+ZTEST(buffer, test_buffer_isr_over_allocate)
+{
+	csp_packet_t *packets[CSP_BUFFER_COUNT];
+	csp_packet_t *p;
+	int i;
+
+	memset(packets, 0, sizeof(packets));
+
+	for (i = 0; i < CSP_BUFFER_COUNT; i++) {
+		packets[i] = csp_buffer_get_isr(0);
+		zassert_true(packets[i] != NULL, NULL);
+	}
+
+	zassert_true(csp_buffer_remaining() == 0);
+	p = csp_buffer_get_isr(0);
+	zassert_true(p == NULL, NULL);
+
+	for (i = 0; i < CSP_BUFFER_COUNT; i++) {
+		csp_buffer_free_isr(packets[i]);
+	}
+}
+
 ZTEST_SUITE(buffer, NULL, setup, NULL, NULL, NULL);
